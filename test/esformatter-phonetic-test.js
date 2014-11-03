efd77f0456b68ce808fd6391aabf57da51184aaf
Todd Wolfson <todd@twolfson.com>
2014-11-03 04:16:47 -0800

    Wrote out gist of test

--- test/esformatter-phonetic-test.js
+++ test/esformatter-phonetic-test.js
@@ -2,10 +2,31 @@
 var esformatter = require('esformatter');
 var esformatterPhonetic = require('../');
 
+// Register our plugin
+esformatter.register(esformatterPhonetic);
+
+// Define test utilities
+var testUtils = {
+  format: function (filepath) {
+    before(function formatFn () {
+      // Format our content
+      var input = fs.readFileSync(filepath, 'utf8');
+      this.output = esformatter.format(input);
+    });
+    after(function cleanup () {
+      // Cleanup output
+      delete this.output;
+    });
+  }
+};
+
+// Start our tests
 describe('esformatter-phonetic', function () {
   describe('formatting a JS file with single variable definitions', function () {
-    it('updates the names', function () {
+    testUtils.format(__dirname + '/test-files/single-variables.js');
 
+    it('updates the names', function () {
+      // TODO: Generate a seed and stick to it
     });
   });
 });
