e472aaef446128a7d8e73e0cca3b40c90ebb0fbf
Todd Wolfson <todd@twolfson.com>
2014-11-03 04:11:00 -0800

    Starting to elaborate on test case

--- test/esformatter-phonetic-test.js
+++ test/esformatter-phonetic-test.js
@@ -1,11 +1,11 @@
+// Load in dependencies
+var esformatter = require('esformatter');
 var esformatterPhonetic = require('../');
 
 describe('esformatter-phonetic', function () {
-  before(function () {
+  describe('formatting a JS file with single variable definitions', function () {
+    it('updates the names', function () {
 
-  });
-
-  it('', function () {
-
+    });
   });
 });
